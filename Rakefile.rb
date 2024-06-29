471a3690da3c2e5c124a7d58057b2461210cc1dd
0x1eef <0x1eef@protonmail.com>
2024-06-29 19:57:28 -0300

    Add 'rake ci'

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,12 +1,20 @@
 require "bundler/setup"
 
 namespace :format do
-  desc "Run rubocop"
-  task :ruby do
+  desc "Apply rubocop fixes"
+  task :apply do
     sh "bundle exec rubocop -A"
   end
+
+  desc "Run rubocop"
+  task :check do
+    sh "bundle exec rubocop"
+  end
 end
-task format: %w[format:ruby]
+task format: %w[format:apply]
+
+desc "Run CI tasks"
+task ci: %i[format:check test]
 
 desc "Run tests"
 task :test do
